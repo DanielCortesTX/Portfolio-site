d2ca78ed941e09d65b75d59514092a5645c7b60b
Daniel Cortes <d5t5c5@yahoo.com>
2019-04-30 12:26:34 -0500

    feat: Added text overlay for project links

--- src/components/ProjectLink.js
+++ src/components/ProjectLink.js
@@ -3,7 +3,12 @@ import React from 'react'
 export default function ProjectLink(props) {
   return (
     <div className="card p-2 project-link">
-      <img src={props.site.image} alt="" className="pic-thumb"/>
+      <div className="card">
+        <img src={props.site.image} alt="" className="pic-thumb"/>
+        <div className="link-overlay">
+          <p className="lead p-1 text-white">{props.site.description}</p>
+        </div>
+      </div>
       {props.site.title}
       <a href={props.site.link} target="_blank" rel="noopener noreferrer" className="btn btn-primary m-1">View</a>
     </div>
