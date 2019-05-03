9297d88df083eb3c3700e056a9d71451c7c3e942
Daniel Cortes <d5t5c5@yahoo.com>
2019-05-03 11:11:52 -0500

    feat: Rearranged layout of project link

--- src/components/ProjectLink.js
+++ src/components/ProjectLink.js
@@ -3,15 +3,17 @@ import React from 'react'
 export default function ProjectLink(props) {
   return (
     <div className="card p-3 project-link">
-      <div className="card mb-4">
+      {props.site.title}
+      <div className="card my-4">
         <img src={props.site.image} alt="" className="pic-thumb"/>
         <div className="link-overlay">
           <p className="lead p-1 text-white">{props.site.description}</p>
         </div>
       </div>
-      {props.site.title}
-      <a href={props.site.link} target="_blank" rel="noopener noreferrer" className="btn btn-primary mt-4 mb-1">View Site</a>
-      <a href={props.site.github} target="_blank" rel="noopener noreferrer" className="btn btn-success mb-1">View Code</a>
+      <div className="d-flex flex-row justify-content-around">
+        <a href={props.site.link} target="_blank" rel="noopener noreferrer" className="btn btn-primary mb-1">View Site</a>
+        <a href={props.site.github} target="_blank" rel="noopener noreferrer" className="btn btn-success mb-1">View Code</a>
+      </div>
     </div>
   )
 }
