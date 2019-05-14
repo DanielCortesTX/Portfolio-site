7af59037c9d9f6cc5a0ae422cfe5e0daa69aa09b
Daniel Cortes <d5t5c5@yahoo.com>
2019-05-13 20:39:03 -0500

    feat: Made contact links open on new page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ function Footer() {
   return (
     <div className="text-white bg-dark p-4">
     <div>
-    <a href="https://github.com/DanielCortesTX" className="p-2">
+    <a href="https://github.com/DanielCortesTX" target="_blank" rel="noopener noreferrer" className="p-2">
       <i className="fab fa-github fa-2x contact-link"></i>
     </a>  
-    <a href="https://www.linkedin.com/in/daniel-cortes-webdev/" className="p-2">
+    <a href="https://www.linkedin.com/in/daniel-cortes-webdev/" target="_blank" rel="noopener noreferrer" className="p-2">
       <i className="fab fa-linkedin-in fa-2x contact-link"></i>
     </a>    
   </div>
