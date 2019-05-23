0b011933dbd9471d74386f67388a3d659a6c392b
Daniel Cortes <d5t5c5@yahoo.com>
2019-05-23 14:59:06 -0500

    feat: Removed superfluous import of contact

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import About from './components/About'
 import Projects from './components/Projects'
-import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Loading from './components/misc/Loading'
 import Skills from './components/Skills'
