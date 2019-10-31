14f6c2bdb33e5462704a310431f70e6360995427
unknown <dcorteswebdev@gmail.com>
2019-10-31 09:56:23 -0500

    feat: Updated skills layout and properties

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,31 +6,35 @@ export default function Skills() {
       <h1 className="display-4">Skills</h1>
       <div className="d-flex flex-row flex-wrap mb-3 justify-content-around p-4">
         <div className="skill-block skills p-3">
-          <p className="mb-0 list-header">General</p>
+          <p className="mb-0 list-header">Advanced</p>
           <hr className="line-hr"/>
           <ul className="d-flex flex-column  lead skills-list">
             <li>HTML5</li>
             <li>CSS3</li>
-            <li>Javascript (ES6)</li>
+            <li>Javascript (ES8)</li>
+            <li>MongoDB</li>
+            <li>Express.js</li>
+            <li>React.js</li>
+            <li>Redux</li>
           </ul>
         </div>
         <div className="skill-block p-3">
-          <p className="mb-0 list-header">Frameworks</p>
+          <p className="mb-0 list-header">Intermediate</p>
           <hr className="line-hr"/>
           <ul className="d-flex flex-column lead skills-list">
-            <li>Express.js</li>
-            <li>React.js</li>
-            <li>AngularJs</li>
-            <li>Angular 2</li>
+            <li>SCSS</li>
+            <li>Webpack</li>
+            <li>Bootstrap 4</li>
           </ul> 
         </div>
         <div className="skill-block skills p-3">
-          <p className="mb-0 list-header">Misc</p>
+          <p className="mb-0 list-header">Familiar</p>
           <hr className="line-hr"/>
           <ul className="d-flex flex-column lead skills-list">
-            <li>MongoDB</li>
-            <li>Redux</li>
-            <li>Webpack</li>
+            <li>Angular 2</li>
+            <li>Angular Js</li>
+            <li>PHP</li>
+            <li>MySQLi</li>
           </ul> 
         </div>
       </div>  
